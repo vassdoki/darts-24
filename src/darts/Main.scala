c7f064c5c0aa51ee476e00ba8a36bffe54c65669
vassdoki <laszlo.vass@imind.eu>
2016-08-09 13:51:29 +0200

    Number 6 recognition error fixed.

--- src/darts/Main.scala
+++ src/darts/Main.scala
@@ -535,7 +535,7 @@ object Main extends SimpleSwingApplication {
     val nums = List(6, 13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10)
 
     val int: Int = Math.floor((degree + 9) / 18).toInt
-    val number = if (int > 19) 0 else nums(int)
+    val number = if (int > 19) nums(0) else nums(int)
 
     val circleNumber: Int = distancesFromBull filter { dfb => dfb < distance } length
 
